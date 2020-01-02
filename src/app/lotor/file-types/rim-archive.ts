37e6cd697d94b1fde025d89afa757bfd20e56011
Scott Huffman <scott@shuffman.com>
2020-01-02 10:31:08 -0500

    building file tree based on dir structure

---
+++ src/app/lotor/file-types/rim-archive.ts
@@ -0,0 +1,160 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { FileExtensions } from '../file-extensions';
+import { Archive } from './archive';
+import { RimFile } from './rim-file';
+
+
+interface RimHeader {
+	type: string;
+	version_string: string;
+	entry_count: number;
+	offset_to_key_list: number;
+	offset_to_resource_list: number;
+}
+interface RimKey {
+	fileName: string;
+	res_id: number;
+	fileExtension: string;
+}
+
+export class RimArchive extends Archive {
+	static sizes = {
+		header: 44,
+		key: 24,
+		resource: 8,
+	};
+
+	fileExtension = 'rim';
+
+	header: RimHeader;
+
+	fd: number;
+	files: RimFile[];
+
+	constructor(public fileName: string, private archivePath: string, private game: 'KOTOR' | 'TSL') {
+		super();
+
+		this.readHeader();
+		this.parseFileList();
+	}
+
+	open() {
+		// already opened, nothing to do
+		if (this.fd) {
+			return false;
+		}
+
+		this.fd = fs.openSync(this.archivePath, 'r');
+		// this call opened the file, indicate that by returning true
+		return true;
+	}
+	close(opened = true) {
+		// if false value given, caller did not actually open
+		if (!opened) {
+			return false;
+		}
+		fs.closeSync(this.fd);
+		this.fd = null;
+	}
+
+	load() {
+		const opened = this.open();
+
+		this.readHeader();
+		// if (this.header.language_count) {
+		// 	this.readStrings();
+		// }
+		this.parseFileList();
+
+		this.close(opened);
+	}
+	readHeader() {
+		const opened = this.open();
+
+		const buffer = Buffer.alloc(RimArchive.sizes.header);
+		fs.readSync(this.fd, buffer, 0, RimArchive.sizes.header, 0);
+
+		this.header = {
+			type: buffer.toString('utf-8', 0, 4).trim().toLowerCase(),
+			version_string: buffer.toString('utf-8', 4, 8).trim().toLowerCase(),
+			entry_count: buffer.readUInt32LE(12),
+			offset_to_resource_list: buffer.readUInt32LE(16),
+			offset_to_key_list: 120
+		};
+
+		this.close(opened);
+	}
+	// readStrings() {
+	// 	const opened = this.open();
+
+	// 	const buffer = Buffer.alloc(this.header.localized_string_size);
+
+	// 	fs.readSync(
+	// 		this.fd, buffer, 0,
+	// 		this.header.entry_count * (RimArchive.sizes.key + RimArchive.sizes.resource),
+	// 		this.header.offset_to_key_list
+	// 	);
+
+
+	// 	// read a string
+	// 	let lang_id = buffer.readUInt32LE(0);
+	// 	let feminine = false;
+	// 	if (lang_id % 2) {
+	// 		feminine = true;
+	// 		lang_id -= 1;
+	// 	}
+	// 	lang_id /= 2;
+	// 	// TODO select an encoding based on language ID
+	// 	// let str_size = buf.readUInt32LE(4, 8);
+	// 	// let s = buf.slice(8, 8 + str_size);
+	// 	// if (s.charCodeAt(s.length - 1) === 0) {
+	// 	// 	s = s.slice(0, -1);
+	// 	// }
+
+	// 	this.close(opened);
+	// }
+	parseFileList() {
+		const opened = this.open();
+
+		this.files = [];
+
+		if (!this.header.entry_count) {
+			return;
+		}
+		const buffer = Buffer.alloc(this.header.entry_count * (RimArchive.sizes.key + RimArchive.sizes.resource));
+		fs.readSync(
+			this.fd, buffer, 0,
+			this.header.entry_count * (RimArchive.sizes.key + RimArchive.sizes.resource),
+			this.header.offset_to_key_list
+		);
+
+		this.files = [];
+		for (let i = 0; i < this.header.entry_count; i++) {
+			const keypos = i * RimArchive.sizes.key;
+
+			const fileName = buffer.toString('utf-8', keypos, keypos + 16).replace(/\0+$/, '');
+			const res_id = buffer.readUInt32LE(keypos + 16);
+
+			const key: RimKey = {
+				fileName,
+				res_id,
+				fileExtension: FileExtensions[`${buffer.readUInt16LE(keypos + 20)}`]
+			};
+
+			const respos = this.header.entry_count * RimArchive.sizes.key + (i * RimArchive.sizes.resource);
+
+			const res = new RimFile(
+				`${key.fileName}.${key.fileExtension}`,
+				key.fileExtension,
+				buffer.readUInt32LE(respos),
+				buffer.readUInt32LE(respos + 4),
+			);
+			res.archive = this;
+
+			this.files.push(res);
+		}
+
+		this.close(opened);
+	}
+}
--- src/app/lotor/game.ts
+++ src/app/lotor/game.ts
@@ -1,45 +1,88 @@
-import { Bif, } from './bif';
-import { Erf } from './erf';
+import { Bif } from "./bif";
+import { Erf } from "./erf";
+import * as path from "path";
+import * as fs from "fs";
+import { Rim } from "./rim";
+import { ErfArchive } from "./file-types/erf-archive";
+import { RimArchive } from "./file-types/rim-archive";
 
-import * as fs from 'fs';
+const FILE_TYPES: { [key: string]: any } = {
+	// 'bif': Bif,
+	".erf": ErfArchive,
+	'.rim': RimArchive,
+	'.mod': RimArchive,
+};
 
 export class Game {
 	bif: Bif;
 	erf: Erf;
+	rim: Rim;
 
-	game: 'KOTOR' | 'TSL';
+	game: "KOTOR" | "TSL";
 
 	constructor(dir: string) {
 		const directory = dir;
 
 		if (!fs.existsSync(directory)) {
-			console.log('directory does not exist');
+			console.log("directory does not exist");
 			return;
 		}
 
 		const data = fs.readdirSync(directory);
 
-		const key = data.find((row) => {
-			return row === 'chitin.key';
+		const key = data.find(row => {
+			return row === "chitin.key";
 		});
 
 		if (!key) {
-			console.log('invalid directory');
+			console.log("invalid directory");
 			return;
 		}
 
-		const game = data.find((row) => {
-			return row === 'swkotor2.ini';
+		const game = data.find(row => {
+			return row === "swkotor2.ini";
 		});
 
-		if (game === 'swkotor2.ini') {
-			this.game = 'TSL';
+		if (game === "swkotor2.ini") {
+			this.game = "TSL";
 		} else {
-			this.game = 'KOTOR';
+			this.game = "KOTOR";
 		}
+
+		// console.log(directory);
+		const fList = fs.readdirSync(directory);
+
+		const dirs = fList
+			.map(p => {
+				return {
+					path: path.join(directory, p),
+					dir: p
+				};
+			})
+			.filter(p => fs.lstatSync(p.path).isDirectory())
+			.map(p => {
+				return {
+					...p,
+					files: fs
+						.readdirSync(p.path)
+						.map(f =>
+							FILE_TYPES[path.extname(f)]
+								? new FILE_TYPES[path.extname(f)](
+										f,
+										path.join(p.path, f),
+										this.game
+								)
+								: f
+						)
+				};
+			});
+
+		console.log(dirs);
 
 		this.bif = new Bif(directory, this.game);
 		this.erf = new Erf(directory, this.game);
+		// this.rim = new Rim(directory, this.game);
+
 		// this.erf = new erf(directory, fs);
 	}
 }
